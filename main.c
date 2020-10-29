4cae54e640f8fa7e6590d982ccf51436a1f18019
Dmitry <feddiman02@gmail.com>
2020-10-29 23:31:18 +0300

    Полностью переделал программу по указатели Test

--- main.c
+++ main.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
-int init_array(int height, int width, int arr[height][width], int sim);
-int print_array(int height, int width, int arr[height][width]);
+void init_array(int height, int width, int *parr[], int sim);
+void print_array(int height, int width, int *parr[]);
 
 int main(){
-    int height, width, sim;
-    scanf("%d", &height);
-    scanf("%d", &width);
-    scanf("%d", &sim);
-    int arr[height][width];
-    init_array(height, width, arr, sim);
-    print_array(height, width, arr);
+    int my_array [10][10];
+    init_array(10,10,my_array,0);
+    print_array(10,10,my_array);
+
+    printf("\n");
+
+    init_array(10,10,my_array,1);
+    print_array(10,10,my_array);
+
     return 0;
 }
 
-int init_array(int height, int width, int arr[height][width], int sim){
-    for(int i = 0; i < height; i++)
-        for(int j = 0; j < width; j++)
-            arr[i][j] = sim;
-    return(arr);
+void init_array(int height, int width, int *parr[], int sim){
+    int (*p)[height] = parr;
+    for(int *i = p[0]; i < p[0] + height * width; i++){
+            *i = sim;
+    }
 }
 
-int print_array(int height, int width, int arr[height][width]){
+void print_array(int height, int width, int *parr[]){
+    int (*p)[height] = parr;
     for(int i = 0; i < height; i++){
-        for(int j = 0; j < width; j++)
-            printf("%d", arr[i][j]);
+        for(int *j = p[i]; j < p[i] + width; j++)
+            printf("%d ", *j);
         printf("\n");
     }
-
 }
